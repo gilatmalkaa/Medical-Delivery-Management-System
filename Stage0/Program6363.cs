99667880d00f2083a163f6d28ae136087c5a05ac
Gilat Kedem <gilatkedem@gmail.com>
2025-10-30 17:20:57 +0200

    # Initial Code + Welcome functions #stage 4

---
+++ Stage0/Program6363.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Stage0
+{
+    partial class Program6363
+    {
+        static void Main(string[] args)
+        {
+            Welcome6363();
+            Welcome9172();
+            Console.ReadKey();
+        }
+        static partial void Welcome9172();
+
+        private static void Welcome6363()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    
+    }
+}
---
+++ Stage0/Program9172.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stage0 { 
+    partial class program
+    {
+        static partial Welcome9172()
+        {
+           
+            Console.WriteLine("I am also here!");
+        }
+    }
+}
